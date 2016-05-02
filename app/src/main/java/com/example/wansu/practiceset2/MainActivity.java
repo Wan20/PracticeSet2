904cabf663616b7a5d9a6cab35495d4ed21e6c97
Wan20 <wandy20number@gmail.com>
2016-05-02 21:29:16 +0700

    Program from Udacity

--- app/src/main/java/com/example/wansu/practiceset2/MainActivity.java
+++ app/src/main/java/com/example/wansu/practiceset2/MainActivity.java
@@ -10,14 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int weekday = 5;
-        int weekend = 9;
-        int optimalHours = 7 * 8;
-
-        int actualHours = weekday;
-        actualHours = actualHours + weekend * 2;
-        int solution = optimalHours - actualHours;
-        display(solution);
+        int dollars = 40;
+        int dollarsToYen = 119;
+        int yen = dollarsToYen * dollars;
+        display(yen);
     }
 
     /**
